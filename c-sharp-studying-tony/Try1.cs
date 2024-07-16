b99ce1763cbd8f82b2adfa3382245c8e07ed405c
97254 <97254@DESKTOP-5A7IST6>
2024-07-16 10:25:16 +0300

    Add project files.

---
+++ c-sharp-studying-tony/Try1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_studying_tony
+{
+    public class Try1 
+    {
+        public static void PrintEvens(int numMax)
+        {
+            Console.WriteLine("hello tony");
+            for(int i = 0; i <= numMax; i+=2) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
