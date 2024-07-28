8a1c54f33e26726b23e624dc1e0517ed9ccfab9c
Tony <tonythegass@gmail.com>
2024-07-28 09:09:08 +0300

    Account

---
+++ c-sharp-studying-tony/Account/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_studying_tony.Account
+{
+    internal class Account
+    {
+        public Owner Owner { get; private set; }
+        public double Balance { get; private set; }
+        public int Overdraft { get; private set; }
+        public const int MAX_OVERDRAFT = 90000;
+
+        public Account(Owner owner, double balance, int overdraft)
+        {
+            Owner = owner;
+            Balance = balance;
+            Overdraft = overdraft > MAX_OVERDRAFT ? MAX_OVERDRAFT : overdraft;
+        }
+
+        public void SetOverdraft(int overdraft)
+        {
+            if (overdraft > MAX_OVERDRAFT)
+            {
+                Console.WriteLine($"Cannot set overdraft more than {MAX_OVERDRAFT}");
+            }
+            else
+            {
+                Overdraft = overdraft;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (Balance - amount < -Overdraft)
+            {
+                Console.WriteLine("Withdrawal exceeds overdraft limit. Transaction denied.");
+            }
+            else
+            {
+                Balance -= amount;
+            }
+        }
+    }
+}
---
+++ c-sharp-studying-tony/Account/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_studying_tony.Account
+{
+    public class Owner
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Owner(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
