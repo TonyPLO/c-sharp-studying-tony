3a56129cf59b2ba4a0e07a970b3de1d3abb9f5a5
Tony <tonythegass@gmail.com>
2024-08-06 09:21:17 +0300

    JS

---
+++ c-sharp-studying-tony/html-exmple/Tony-script.js
@@ -0,0 +1,17 @@
+﻿// Change Text Task
+document.getElementById('changeTextButton').addEventListener('click', function () {
+    document.getElementById('text').innerText = 'New Text';
+});
+
+// Change Color Task
+document.getElementById('changeColorButton').addEventListener('click', function () {
+    document.getElementById('colorText').setAttribute('class', 'newColor');
+});
+
+// Input Validation Task
+document.getElementById('validateButton').addEventListener('click', function () {
+    var input = document.getElementById('numberInput').value;
+    var number = +input; // Convert to number
+    var resultText = number > 100 ? "תקין" : "לא תקין";
+    document.getElementById('result').innerText = resultText;
+});
